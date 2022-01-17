345bd6e102439d7190ff3626e16c4a68fa3c31e3
T-REX32 <90477142+T-REX32@users.noreply.github.com>
2022-01-17 15:41:19 -0200

    Update teste.py
    resolvido alguns problemas de digito e codigo

--- teste.py
+++ teste.py
@@ -23,46 +23,44 @@ while True:
 
     dados['endereço'] = input("Endereço: ")
 
-    dados['tipo de despesa'] = str(input("Qual o tipo de despesa? (água, esgoto, luz, gás e condomínio): "))
+    dados['tipo_de_despesa'] = str(input("Qual o tipo de despesa? (água, esgoto, luz, gás e condomínio): "))
 
-    if dados['tipo_de_despesa'] in "água":
+    if dados['tipo_de_despesa'] == "água":
         dados['despesas_da_água'] = float(input("Despesas da água: "))
 
-    elif dados['tipo_de_despesa'] in "esgoto":
+    elif dados['tipo_de_despesa'] == "esgoto":
         dados['despesas_do_esgoto'] = float(input("Despesas do esgoto: "))
 
-    elif dados['tipo_de_despesa'] in "luz":
+    elif dados['tipo_de_despesa'] == "luz":
         dados['despesas_da_luz'] = float(input("Despesas da luz: "))
     
-    elif dados['tipo_de_despesa'] in "gás":
+    elif dados['tipo_de_despesa'] == "gás":
         dados['despesas_do_gás'] = float(input("Despesas do gás: "))
 
-    elif dados['tipo_de_despesa'] in "condomínio":
+    elif dados['tipo_de_despesa'] == "condomínio":
         dados['despesas_do_condomínio'] = float(input("Despesas do condomínio: "))
 
     dados['editar_despesa'] = str(input("Deseja editar suas despesas? "))
 
     if dados['editar_despesa'] in "Ss":
-        dados['tipo de despesa'] = str(input("Qual o tipo de despesa? (água, esgoto, luz, gás e condomínio): "))
+        dados['tipo_de_despesa'] = str(input("Qual o tipo de despesa? (água, esgoto, luz, gás e condomínio): "))
 
-    if dados['tipo_de_despesa'] in "água":
+    if dados['tipo_de_despesa'] == "água":
         dados['despesas_da_água'] = float(input("Despesas da água: "))
 
-    elif dados['tipo_de_despesa'] in "esgoto":
+    elif dados['tipo_de_despesa'] == "esgoto":
         dados['despesas_do_esgoto'] = float(input("Despesas do esgoto: "))
 
-    elif dados['tipo_de_despesa'] in "luz":
+    elif dados['tipo_de_despesa'] == "luz":
         dados['despesas_da_luz'] = float(input("Despesas da luz: "))
     
-    elif dados['tipo_de_despesa'] in "gás":
+    elif dados['tipo_de_despesa'] == "gás":
         dados['despesas_do_gás'] = float(input("Despesas do gás: "))
 
-    elif dados['tipo_de_despesa'] in "condomínio":
+    elif dados['tipo_de_despesa'] == "condomínio":
         dados['despesas_do_condomínio'] = float(input("Despesas do condomínio: "))
     
     dados['vencimento_fatura'] = int(input("Escolha o dia do vencimento de sua fatura (quantos dias para o vencimento): "))
-    while dados['vencimento_fatura'] > 30:
-        print("Não aceitamos mais que 30 dias! escolha até 30 ou menos dias para pagar sua fatura.")
 
     dados['finalizar'] = str(input("Deseja continuar? [S/N]: "))
     if dados['finalizar'] not in "Ss":
